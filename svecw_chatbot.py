b920836c2f720fba894f7810c106eab2b96de51c
Sana8367 <165360648+Sana8367@users.noreply.github.com>
2025-02-28 11:13:47 +0530

    Update svecw_chatbot.py

--- svecw_chatbot.py
+++ svecw_chatbot.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import google.generativeai as genai
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 st.set_page_config(page_title ="Svecw College Chatbot", layout="centered")
 
